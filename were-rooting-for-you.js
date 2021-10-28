0ea660edcb528ad677703a55b5777c455c11d648
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-10-28 07:16:56 +0000

    more exercises

---
+++ were-rooting-for-you.js
@@ -0,0 +1,31 @@
+function judgeVegetable(vegetables, metric) {
+  //Our function must return the name of the person who submitted (vegetables.submitter) 
+  //the vegetable with the highest ranking in the provided category.
+  let maximum = Math.max.apply(Math, vegetables.map(function(o) { return o[metric]; }));
+  for (let i = 0; i < vegetables.length; i++) {
+    if (vegetables[i][metric] === maximum) {
+      return vegetables[i].submitter;
+    } 
+  }
+}
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+console.log(judgeVegetable(vegetables, metric))
